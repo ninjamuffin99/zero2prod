3159a0a0fa25af891cef2f33debd1e3629841bf0
Cameron Taylor <cameron.taylor.ninja@gmail.com>
2022-09-14 13:28:03 -0400

    finish chapter 3.7

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() ->  std::io::Result<()> {
+    run(TcpListener::bind("http://127.0.0.1:0").unwrap())?.await
 }
